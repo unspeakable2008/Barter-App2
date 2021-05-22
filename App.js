42058f63c48e66356f837225b3b256c0c9823619
unspeakable2008 <64965483+unspeakable2008@users.noreply.github.com>
2021-05-22 09:31:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignupScreen from "./screens/SignupScreen"
+export default function App() {
+  return (
+   <WelcomeScreen></WelcomeScreen>
+  );
+}
